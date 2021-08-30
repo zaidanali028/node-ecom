c36f479e8a360a7a51c22d84d3e3157df5f6d79e
zaidanali028 <zaidanali028@gmail.com>
2021-08-30 00:58:42 +0000

    y

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/sitemap.xml", function (req, res) {
 });
 // app.set('trust proxy', true);
 
-const dbUrI =process.env.NODE_ENV === "production"? process.env.DB_URL:"mongodb://localhost:27017/ehop-update"
+const dbUrI =process.env.NODE_ENV === "production"? process.env.DB_URL:"mongodb://localhost:27017/eshop-update"
 
 app.enable("trust proxy"); // trust all
 
